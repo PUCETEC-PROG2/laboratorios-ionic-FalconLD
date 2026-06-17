e217bd1c9389ba4907d06b5405394b25d6416bf8
Usuario invitado <Guest@iMacA10818.PUCE.EDU.EC>
2026-06-17 15:57:53 -0500

    Personalizacion de tab 2 y 3

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,7 +6,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Crear Repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +15,31 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="form-container">
+          <IonInput
+            className="form-field"
+            label="Nombre del repositorio"
+            labelPlacement="floating"
+            placeholder="Ingrese el nombre del repositorio"
+          />
+          <IonTextarea
+            className="form-field"
+            label="Descripcion del repositorio"
+            labelPlacement="floating"
+            placeholder="Ingrese la descripcion del repositorio"
+            rows={6}
+          />
+          <IonButton
+            className="form-field"
+            expand="block"
+            shape="round"
+            color="primary"
+          >
+            Guardar
+
+          </IonButton>
+        </div>
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,7 +6,7 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +15,25 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/200696133?v=4" alt="avatar" />
+            <IonCardHeader>
+               <IonTitle>Leonardo David Falconi Velastegui</IonTitle>
+              <IonCardSubtitle>FalconLD</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardHeader>
+              Desarollador de Software, cinturon negro en Tae Kwon Do, Peak Elo Diamante en League of Legends, en busca de madres solteras de padre ausente
+            </IonCardHeader>
+          </IonCard>
+
+        </div>
+
+
+
+
       </IonContent>
     </IonPage>
   );
